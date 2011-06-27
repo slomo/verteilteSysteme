efab7ea27c6be6b3a1e38e4311f4b990e445cc03
yves <steve.harrison@gmx.net>
2011-06-27 23:59:11 +0200

    messages header file

---
+++ erl/messages.hrl
@@ -0,0 +1,13 @@
+% --- Messages definitions ----------------------------------------------------
+-record(hello,{name}).
+-record(olleh,{name}).
+-record(goods,{src,goods}). % goods is a list of {name,amount} Tupels
+-record(sdoog,{src,goods}).
+
+-record(routed,{routeDone=[],routeTodo=[],content}).
+% contents of routed package
+-record(sreep,{peers}).
+-record(peers,{}).
+-record(cost,{good}).
+-record(tsoc,{good,buyPrice,buyAmount,sellPrice,sellAmount}).
+
