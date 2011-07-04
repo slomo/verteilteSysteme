fc443b62fbf112f8d723f4b9ddb7fe6aceb71e81
yves <steve.harrison@gmx.net>
2011-07-04 13:10:07 +0200

    parsing untested

--- 07-erlang/messages.hrl
+++ 07-erlang/messages.hrl
@@ -1,8 +1,20 @@
 % --- Messages definitions ----------------------------------------------------
 -record(hello,{name}).
 -record(olleh,{name}).
--record(goods,{src,goods}). % goods is a list of {name,amount} Tupels
+-record(bye,{name}).
+-record(eyb,{name}).
+
+-record(whois,{planet}).
+-record(siohw,{planet,addr}).
+
+-record(goods,{src,goods}). % goods is a list of {name,ttl} Tupels
 -record(sdoog,{src,goods}).
+
+-record(buy,{good,amount,price}).
+-record(yub,{good,amount,price}).
+-record(sell,{good,amount,price}).
+-record(lles,{good,amount,price}).
+
 
 -record(routed,{routeDone=[],routeTodo=[],content}).
 % contents of routed package
